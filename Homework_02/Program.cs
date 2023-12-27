03a4b0bc4dd7da0cef36f6fd30ca3049843785ed
Roman Gunger <Romangv93@gmail.com>
2023-12-27 21:59:11 +0300

    init commit

---
+++ Homework_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+        int length = a.GetLength(0);
+
+        var str = new StringBuilder();
+
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                str.Append(a[i,j]);
+            }
+        }
+
+        int[] ints = new int[length * length];
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if(int.TryParse(str[i].ToString(), out int value))
+            {
+                ints[i] = value;
+            }
+        }
+
+        Array.Sort(ints);
+
+        int counter = 0;
+
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                a[i, j] = ints[counter];
+                counter++;
+                Console.Write($"{a[i,j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
